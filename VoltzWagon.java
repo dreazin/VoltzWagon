7d8990eb01edf7436c7754913f65ff39ba4717ca
John Busch Jr <jbuschjr@purdue.edu>
2015-10-03 11:49:37 -0400

    Okay independent radar tracking, need to look a bit more at these physics...

--- VoltzWagon.java
+++ VoltzWagon.java
@@ -1,6 +1,7 @@
 package VoltzWagon;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
+import java.lang.Math;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
@@ -9,11 +10,69 @@ import robocode.*;
  */
 public class VoltzWagon extends Robot
 {
+	private class SeenRobot {
+		private long tickSeen;
+		private double bearing,
+					   distance,
+					   energy,
+					   heading,
+					   velocity;
+			
+		public SeenRobot(ScannedRobotEvent e) {
+			this.update(e);
+		}
+		
+		public void update(ScannedRobotEvent e) {
+			this.tickSeen = getTime();
+			this.bearing = e.getBearing();
+			this.distance = e.getDistance();
+			this.energy = e.getEnergy();
+			this.heading = e.getHeading();
+			this.velocity = e.getVelocity();
+		}
+		
+		public long getTickSeen() {
+			return this.tickSeen;
+		}
+		
+		public double getBearing() {
+			return this.bearing;
+		}
+		
+		public double getDistance() {
+			return this.distance;
+		}
+		
+		public double getEnergy() {
+			return this.energy;
+		}
+		
+		public double getHeading() {
+			return this.heading;
+		}
+		
+		public double getVelocity() {
+			return this.velocity;
+		}
+	}
+	
+	// Only doing 1v1 so we can hard define out target
+	private SeenRobot enemy;
+	
+	// Ticks after which a seen robot isn't reliable anymore
+	private final double TICK_TIMEOUT = 20;
+	
 	/**
 	 * run: VoltzWagon's default behavior
 	 */
 	public void run() {
 		// Initialization of the robot should be put here
+		enemy = null;
+		setScanColor(Color.yellow);
+		// Separate our turns
+		setAdjustRadarForGunTurn(true);
+		setAdjustRadarForRobotTurn(true);
+		setAdjustGunForRobotTurn(true);
 
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
@@ -22,11 +81,37 @@ public class VoltzWagon extends Robot
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			/*----- Update Scanner Position -----*/
+			if (enemy == null) {
+				// No enemy seen yet
+				// Scan clockwise for now, 45 degrees/tick
+				turnRadarRight(45);
+			}
+			else if (getTime() - enemy.getTickSeen() > TICK_TIMEOUT) {
+				// Data is old and unreliable
+			}
+			else {
+				// Take action based on data we have
+				
+				double targAngle = getHeading() + enemy.getBearing();
+				if (targAngle < 0) targAngle += 360;
+				double radarBearing = targAngle - getRadarHeading();
+				System.out.println("Radar bearing:");
+				System.out.println(radarBearing);
+				if (radarBearing < 0) {
+					if (Math.abs(radarBearing) > 45)
+						turnRadarLeft(45);
+					else
+						turnRadarLeft(Math.abs(radarBearing));
+				}
+				else {
+					if (radarBearing > 45)
+						turnRadarRight(45);
+					else
+						turnRadarRight(radarBearing);
+				}	
+			}
+			ahead(20);
 		}
 	}
 
@@ -35,7 +120,12 @@ public class VoltzWagon extends Robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
-		fire(1);//
+		if (enemy == null)
+			enemy = new SeenRobot(e);
+		else
+			enemy.update(e);
+		
+		
 	}
 
 	/**
@@ -52,5 +142,12 @@ public class VoltzWagon extends Robot
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
 		back(20);
-	}	
+	}
+	
+	// Fires every tick
+	public void onStatus(StatusEvent e) {
+		RobotStatus status = e.getStatus();
+		
+	}
+	
 }
